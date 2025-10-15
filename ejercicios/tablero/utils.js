6415457a451dc7f091f27682785faccd4c2b391a
José Manuel GV <jmgv.ciencia@gmail.com>
2025-10-15 16:33:31 +0200

    Añade la función barajarLista para mezclar elementos de una lista

---
+++ ejercicios/tablero/utils.js
@@ -0,0 +1,10 @@
+export { barajarLista } ;
+
+
+function barajarLista(lista) {
+    for (let i = lista.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [lista[i], lista[j]] = [lista[j], lista[i]];
+    }
+    return lista;
+}
