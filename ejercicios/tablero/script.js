dfa8a311227102649369e7ea55452853644bca6c
José Manuel GV <jmgv.ciencia@gmail.com>
2025-10-15 12:40:03 +0200

    Añade estilos de Glassmorphism al menú y al tablero, y mejora la lógica de visibilidad en el script

--- ejercicios/tablero/script.js
+++ ejercicios/tablero/script.js
@@ -1,9 +1,36 @@
 const botonInicio = document.getElementById('inicio');
+const botonCancelar = document.getElementById('cancelar');
 const header = document.getElementById('header');
+const main = document.getElementById('main');
 
-botonInicio.addEventListener('click', desplazarElementos);
+botonInicio.addEventListener('click', inicio);
+botonCancelar.addEventListener('click',cancelar)
 
-function desplazarElementos() {
+
+function inicio(){
+    ocultarHeader();
+    mostrarMain();
+}
+
+function cancelar(){
+    ocultarMain();
+    mostrarHeader();
+}
+
+
+function ocultarHeader() {
     header.classList.add('header--noVisible');
     header.classList.remove('header--visible');
+}
+function ocultarMain() {
+    main.classList.add('main--noVisible');
+    main.classList.remove('main--visible');
+}
+function mostrarHeader() {
+    header.classList.add('header--visible');
+    header.classList.remove('header--noVisible');
+}
+function mostrarMain() {
+    main.classList.add('main--visible');
+    main.classList.remove('main--noVisible');
 }
