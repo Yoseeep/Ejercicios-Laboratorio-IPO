ded18da77a6d6f04986f0b6d580cf12b02ba641a
José Manuel GV <jmgv.ciencia@gmail.com>
2025-11-18 00:58:40 +0100

    Refactoriza el  settings menu layout y  mejora los controles del octave input

--- ejercicios/piano/script.js
+++ ejercicios/piano/script.js
@@ -2,6 +2,10 @@ const settings__menu = document.getElementById('settings__menu');
 const settings__closer = document.getElementById('settings__closer-btn');
 const settings__opener = document.getElementById('settings__opener-btn');
 const settings__darkMode = document.getElementById('settings__dark-mode');
+const octaves__more = document.getElementById('setting-octaves__plus-btn');
+const octaves__less = document.getElementById('settings-octaves__minus-btn');
+const num_octaves = document.getElementById('input-octaves');
+
 
 // Settings menu open/close
 settings__opener.addEventListener('click', () => {
@@ -24,3 +28,31 @@ settings__darkMode.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
 
+// Octaves input number control
+octaves__more.addEventListener('click', () => {
+    if (Number(num_octaves.value) === 1) {
+        octaves__less.disabled = false;
+    }
+    if (Number(num_octaves.value) < 7) {
+        num_octaves.value = Number(num_octaves.value) +  1;
+        document.documentElement.style.setProperty('--numOctaves', num_octaves.value);
+        console.log(document.documentElement.style.getPropertyValue('--numOctaves'));
+    }
+    if (Number(num_octaves.value) === 7) {
+        octaves__more.disabled = true;
+    }
+})
+
+octaves__less.addEventListener('click', () => {
+    if (Number(num_octaves.value) === 7) {
+        octaves__more.disabled = false;
+    }
+    if (Number(num_octaves.value) > 1) {
+        num_octaves.value = Number(num_octaves.value) -  1;
+        document.documentElement.style.setProperty('--numOctaves', num_octaves.value);
+        console.log(document.documentElement.style.getPropertyValue('--numOctaves'));
+    }
+    if (Number(num_octaves.value) === 1) {
+        octaves__less.disabled = true;
+    }
+})
